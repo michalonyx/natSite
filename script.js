cb84e5b177347283a33506c8500e344924008108
michalonyx <michalonyx@gmail.com>
2020-06-30 21:41:19 +0200

    fixing minor bugs

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function showAll() {
     document.querySelector('.menu-button--close').style.display = 'none'
     document.querySelector('.menu-button--about').style.display = 'block'
     document.querySelector('.natSite-about').style.display = 'none'
+    document.querySelector('.ministerstwo').style.display = 'block'
+    document.querySelector('.natSite-body').style.display = 'block'
 }
 
 function showAbout() {
     document.querySelector('.menu-button--about').style.display = 'none'
     document.querySelector('.menu-button--close').style.display = 'block'
     document.querySelector('.natSite-about').style.display = 'flex'
+    document.querySelector('.ministerstwo').style.display = 'none'
+    document.querySelector('.natSite-body').style.display = 'none'
 }
